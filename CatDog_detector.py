2263656e44c1af7a93b761457290e2858a253da6
Eliya Lotfi <mr.eliyalotfi@gmail.com>
2022-08-15 17:32:12 +0430

    Define a function to train test split the image data

--- CatDog_detector.py
+++ CatDog_detector.py
@@ -9,3 +9,39 @@ from sklearn.linear_model import LogisticRegression,SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
 from joblib import dump
+
+def load_data():
+    data_list= []
+    labels=[]
+
+    for i,address in enumerate(glob.glob("image\\*\\*")): 
+        try:
+            img = cv2.imread(address)
+            rgb_img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            m,n,c= rgb_img.shape
+            img_r=np.reshape(rgb_img,(m,n*c))
+            pca=PCA(n_components=10).fit(img_r)
+            img_r=pca.transform(img_r)       
+            img_r=img_r.flatten()
+            x=img_r.size
+            c=x//10
+            img_r=np.reshape(img_r,(10,c))
+            pca=PCA(n_components=10).fit(img_r)
+            img_r=pca.transform(img_r)
+            x= img_r.max()
+            img_r=img_r/x
+            img_r=img_r.flatten()
+            
+            data_list.append(img_r)
+
+            label = address.split("\\")[1]
+            labels.append(label)
+            if i%100 == 0:
+                print("statue: {}/1900 processed".format(i))
+        except:
+            print("Error")
+            continue
+    data_list=np.array(data_list)
+    x_train,x_test,y_train,y_test=train_test_split(data_list,labels,test_size=0.2,random_state=0)
+    return x_train,x_test,y_train,y_test
+    
